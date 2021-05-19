2ac5fb1e44274a4c9ff0b6ea701562560a62544f
kundan-kumar1993 <kundan.rai@evry.com>
2021-05-19 16:00:27 +0530

    all files

---
+++ Abstractions/IVideoService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VideoChat.Models;
+
+namespace VideoChat.Abstractions
+{
+    public interface IVideoService
+    {
+        string GetTwilioJwt(string identity);
+        Task<IEnumerable<RoomDetails>> GetAllRoomsAsync();
+    }
+}
